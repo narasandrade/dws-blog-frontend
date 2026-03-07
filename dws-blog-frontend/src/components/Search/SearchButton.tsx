83f3da54fd859e6c58823de409bce65677d20e2d
Nara Andrade <andradesouzanara@gmail.com>
2026-03-07 14:13:42 -0300

    chore: prevent css leaking

--- dws-blog-frontend/src/components/Search/SearchButton.tsx
+++ dws-blog-frontend/src/components/Search/SearchButton.tsx
@@ -3,7 +3,7 @@ import searchIcon from "@/assets/search.png";
 
 export function SearchButton() {
   return (
-    <button>
+    <button className="search-button">
       <img src={searchIcon} alt="Search" />
     </button>
   );
