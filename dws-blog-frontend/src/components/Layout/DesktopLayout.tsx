b20050412258ed2ff47a626a5308dd18c0797349
Nara Andrade <andradesouzanara@gmail.com>
2026-03-08 00:39:30 -0300

    feat: add title and sort button to desktop layout

--- dws-blog-frontend/src/components/Layout/DesktopLayout.tsx
+++ dws-blog-frontend/src/components/Layout/DesktopLayout.tsx
@@ -1,12 +1,28 @@
+import "./DesktopLayout.scss";
 import dentsuLogo from "@/assets/dentsu-world-services.png";
 import { SearchInput } from "@/components/Search";
+import { SortButton } from "@/components/Sort";
 
 export function DesktopLayout() {
   return (
-    <header>
-      <img src={dentsuLogo} alt="Dentsu world services" />
+    <>
+      <header className="layout-top-bar">
+        <img src={dentsuLogo} alt="Dentsu world services" />
 
-      <SearchInput />
-    </header>
+        <SearchInput />
+      </header>
+
+      <main>
+        <header>
+          <h1>DWS blog</h1>
+
+          <div className="sort-container">
+            <span>Sort by:</span>
+
+            <SortButton />
+          </div>
+        </header>
+      </main>
+    </>
   );
 }
