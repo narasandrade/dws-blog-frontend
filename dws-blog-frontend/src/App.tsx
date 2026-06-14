cb28d842940477224c52d98a11fc8d2395efad0d
Nara Andrade <andradesouzanara@gmail.com>
2026-06-14 14:10:05 -0300

    feat: create routes to app pages

--- dws-blog-frontend/src/App.tsx
+++ dws-blog-frontend/src/App.tsx
@@ -1,10 +1,20 @@
-import { Home } from "./pages/home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { RootLayout } from "./components/RootLayout";
+import { Home } from "./pages/Home";
+import { PostDetails } from "./pages/PostDetails";
 import AppProviders from "./providers/AppProviders";
 
 function App() {
   return (
     <AppProviders>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<RootLayout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/posts/:id" element={<PostDetails />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </AppProviders>
   );
 }
