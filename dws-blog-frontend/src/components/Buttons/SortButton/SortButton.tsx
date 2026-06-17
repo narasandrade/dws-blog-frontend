05cab4b38882fa23f8bd76e92b47f9510bc681b5
Nara Andrade <andradesouzanara@gmail.com>
2026-06-17 15:08:28 -0300

    feat: prepare sort button to change its state and label

--- dws-blog-frontend/src/components/Buttons/SortButton/SortButton.tsx
+++ dws-blog-frontend/src/components/Buttons/SortButton/SortButton.tsx
@@ -2,10 +2,15 @@ import sortIconDefault from "@/assets/sort-accent-medium.png";
 import sortIconHover from "@/assets/sort-neutral-extra-light.png";
 import "./SortButton.scss";
 
-export function SortButton() {
+type Props = {
+  sortOrder: "newest" | "oldest";
+  onToggle: () => void;
+};
+
+export function SortButton({ sortOrder, onToggle }: Props) {
   return (
-    <button className="sort-button">
-      <span>Newest first</span>
+    <button className="sort-button" onClick={onToggle}>
+      <span>{sortOrder === "newest" ? "Newest first" : "Oldest first"}</span>
 
       <img className="default-icon" src={sortIconDefault} alt="Sort" />
       <img className="hover-icon" src={sortIconHover} alt="Sort" />
